26af1e5d553b992981fae4bd0390b423100f9d9c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-01-28 07:57:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+
+// assets
+import bgVideo from "./assets/earth-bg.mp4";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="">Hello,world</h1>
+    <div className="">
+      <div className="h-[700px] relative">
+        <video
+          autoPlay
+          loop
+          muted
+          className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1] "
+        >
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <Navbar />
+      </div>
     </div>
   );
 };
