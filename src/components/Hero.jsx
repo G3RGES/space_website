2faa21c06ddd5fba7db88c200fcbb626f9e95bbf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-01-28 08:18:59 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,16 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="bg-black/20 h-full">
+      <div className="h-full">
+        <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
