67e22a0cd948326abf38df396c442ab7c0a1c138
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-01-28 08:23:52 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="bg-black/20 h-full">
-      <div className="h-full">
+    <div className="bg-black/20 h-full text-white relative z-50">
+      <div className="h-full flex justify-center items-center p-4 ">
         <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className=""></div>
+          <div className="">
+            <h1 className="text-5xl font-bold">Orbite The Earth</h1>
+            <p className=""></p>
+          </div>
           <div className=""></div>
         </div>
       </div>
