aed231b7ed363ef772a3f52b777a86e258fea46b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-01-31 08:13:16 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,24 @@ const Banner = () => {
   return (
     <div className="bg-black text-white pb-12 ">
       <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center"></div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center ">
+          <div className="">
+            <img className="" src={sateliteImg} alt="satelite_img" />
+          </div>
+          <div className="">
+            <p className="text-sky-800 uppercase">Our Mission</p>
+            <h1 className="">Rapidscat</h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              molestiae reprehenderit expedita corporis, non doloremque.
+              Consequatur consectetur quisquam qui sunt. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Eos molestiae reprehenderit
+              expedita corporis, non doloremque. Consequatur consectetur
+              quisquam qui sunt.
+            </p>
+            <button className="">Learn More</button>
+          </div>
+        </div>
       </div>
     </div>
   );
