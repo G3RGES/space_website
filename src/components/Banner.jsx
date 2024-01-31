77fd1466207144562e12c2c8d55600f6c3b84229
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-01-31 07:59:53 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// assets
+import sateliteImg from "../assets/satelite1.jpg";
+
 const Banner = () => {
   return (
-    <div className="">
-      <div className=""></div>
+    <div className="bg-black text-white pb-12 ">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center"></div>
+      </div>
     </div>
   );
 };
