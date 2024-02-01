dbecc2f6ac090ea90bf0aa0d43aa2d0a27a55c0f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-01 07:37:09 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,32 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="">
-      <div className="container"></div>
+    <div className="bg-black text-white border-t-2 border-t-white">
+      <section className="max-w-[1200px] mx-auto  ">
+        <div className="grid md:grid-cols-3 py-5 ">
+          {/* first column */}
+          <div className="">
+            <h1 className="">Be Ready To Grow</h1>
+            <p className="">
+              Get exclusive {""}
+              <span className="text-white font-bold ">updates</span> straight to
+              your inbox.{" "}
+            </p>
+            <br />
+            <div className="">
+              <input
+                type="text"
+                className="py-1 px-3 w-full h-[100%] inline-block focus:outline-none
+                 focus:border-sky-500 focus:ring-2
+                 focus:ring-sky-500 bg-gray-800 border-gray-200 border-2 rounded-lg"
+                placeholder="Email"
+              />
+              <button className="">OK</button>
+            </div>
+          </div>
+          {/* second column */}
+        </div>
+      </section>
     </div>
   );
 };
