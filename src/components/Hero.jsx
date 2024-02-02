d388e73567e60c131d6bcf61aa9a606b29093186
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-02 10:19:58 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,14 +11,22 @@ const Hero = () => {
         <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
           {/* hero section */}
           <div className="space-y-4 lg:pr-36">
-            <h1 className="text-5xl font-bold uppercase">Orbite The Earth</h1>
-            <p className="">
+            <h1 data-aos="fade-right" className="text-5xl font-bold uppercase">
+              Orbite The Earth
+            </h1>
+            <p className="" data-aos="fade-right" data-aos-delay="300">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ea
               dolorem eius accusamus beatae. Nulla quis beatae quo, possimus
               tempora similique dignissimos repellat aperiam veniam culpa
               consequatur repudiandae asperiores saepe.
             </p>
-            <button className="primary-btn ">Learn More</button>
+            <button
+              className="primary-btn"
+              data-aos="fade-right"
+              data-aos-delay="500"
+            >
+              Learn More
+            </button>
           </div>
           <div className=""></div>
         </div>
